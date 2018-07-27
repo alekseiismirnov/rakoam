f1d322074ad94557f059a4bf5e19e586cfb1064a
Alekseii Smirnov <alekseii.sm@gmail.com>
2018-07-27 08:19:08 +0300

    remove redundant block in spec

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -3,35 +3,33 @@ require 'yaml'
 config = YAML.load_file('config.yml')
 raise 'rspec: ATM config is nil' if config.nil?
 
-describe Atm do
-  describe '#login' do
-    before(:each) do
-      @atm = Atm.new(config)
-    end
-    it 'allows existing user to login' do
-      expect(@atm.login(3321, 'mypass')).to eq('Hello, Volodymyr!')
-    end
-    it 'rejects wrong login' do
-      message = @atm.login(321, 'mypass')
-      expect(message).to eq('ERROR: ACCOUNT NUMBER AND PASSWORD DON\'T MATCH')
-    end
-    it 'rejects wrong password' do
-      message = @atm.login('Volodymyr', 'shmass')
-      expect(message).to eq('ERROR: ACCOUNT NUMBER AND PASSWORD DON\'T MATCH')
-    end
+describe '#login' do
+  before(:each) do
+    @atm = Atm.new(config)
   end
+  it 'allows existing user to login' do
+    expect(@atm.login(3321, 'mypass')).to eq('Hello, Volodymyr!')
+  end
+  it 'rejects wrong login' do
+    message = @atm.login(321, 'mypass')
+    expect(message).to eq('ERROR: ACCOUNT NUMBER AND PASSWORD DON\'T MATCH')
+  end
+  it 'rejects wrong password' do
+    message = @atm.login('Volodymyr', 'shmass')
+    expect(message).to eq('ERROR: ACCOUNT NUMBER AND PASSWORD DON\'T MATCH')
+  end
+end
 
-  describe '#user_logged?' do
-    before(:each) do
-      @atm = Atm.new(config)
-    end
-    it 'true on if somebody logged' do
-      @atm.login(3321, 'mypass')
-      expect(@atm.user_logged?).to be_truthy
-    end
-    it 'false on if nobody logged' do
-      @atm.login('jam', 'mypass')
-      expect(@atm.user_logged?).to be_falsey
-    end
+describe '#user_logged?' do
+  before(:each) do
+    @atm = Atm.new(config)
+  end
+  it 'true on if somebody logged' do
+    @atm.login(3321, 'mypass')
+    expect(@atm.user_logged?).to be_truthy
+  end
+  it 'false on if nobody logged' do
+    @atm.login('jam', 'mypass')
+    expect(@atm.user_logged?).to be_falsey
   end
 end
